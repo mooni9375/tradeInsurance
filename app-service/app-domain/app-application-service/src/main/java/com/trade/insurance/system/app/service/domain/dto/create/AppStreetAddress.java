f3a7034bd9f244ea44030c61aed5d0a0abf891a8
mooni9375 <moonmoon9375@gmail.com>
2024-11-13 08:36:49 +0900

    Apply JPA Dependencies and JPA Settings.

--- app-service/app-domain/app-application-service/src/main/java/com/trade/insurance/system/app/service/domain/dto/create/AppStreetAddress.java
+++ app-service/app-domain/app-application-service/src/main/java/com/trade/insurance/system/app/service/domain/dto/create/AppStreetAddress.java
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 @Builder
--- app-service/app-domain/app-application-service/src/main/java/com/trade/insurance/system/app/service/domain/ports/output/repository/AppRepository.java
+++ app-service/app-domain/app-application-service/src/main/java/com/trade/insurance/system/app/service/domain/ports/output/repository/AppRepository.java
@@ -2,7 +2,6 @@ package com.trade.insurance.system.app.service.domain.ports.output.repository;
 
 import com.trade.insurance.system.app.service.domain.entity.App;
 import com.trade.insurance.system.app.service.domain.valueobject.TrackingId;
-import com.trade.insurance.system.domain.valueobject.AppId;
 
 import java.util.Optional;
 
